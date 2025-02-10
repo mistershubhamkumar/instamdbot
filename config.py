2f24b7dc4554d71bd3fcb4394690f49673527a05
Shubham Kumar <kumaruikeshubham@gmail.com>
2025-02-10 07:13:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-SESSION_ID = "आपका_इंस्टाग्राम_सेशन_आईडी"
-BOT_OWNER = "आपका_इंस्टाग्राम_यूजरनेम"
+SESSION_ID = "32436384391%3Aj7o2fW565YSqf8%3A6%3AAYfcXioIgNj0hoNp0dYJ5Hx3A2hCgAI0Fr1DhoC5Gw"
+BOT_OWNER = "msr_shubham.k"
 COMMAND_PREFIX = "."  # बॉट के कमांड इस प्रीफिक्स से शुरू होंगे
